13260b8b739a78e92c76678cc03fbfff4c98ef57
Leonardo Betetto <60801645+lmbbetetto@users.noreply.github.com>
2023-08-10 20:10:12 -0300

    Adicionar arquivos de projeto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace atendimentoAlunos.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
