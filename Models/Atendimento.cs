3694aca9d0f9de6ac00e0f49006310ef1ef68d4e
Leonardo Betetto <60801645+lmbbetetto@users.noreply.github.com>
2023-08-24 19:47:29 -0300

    Commit

--- Models/Atendimento.cs
+++ Models/Atendimento.cs
@@ -1,10 +1,26 @@
-﻿namespace atendimentoAlunos.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace atendimentoAlunos.Models
 {
     public class Atendimento
     {
+        [Key]
+        [Display(Name = "Código")]
         public int id { get; set; }
+
+        public int alunoID { get; set; }
+        [ForeignKey("alunoID")]
+        [Display(Name = "Código do Aluno")]
         public Aluno aluno { get; set; }
+
+        public int salaID { get; set; }
+        [ForeignKey("salaID")]
+        [Display(Name = "Código da Sala")]
         public Sala sala { get; set; }
+
+        [DataType(DataType.DateTime)]
+        [Display(Name = "Data e Hora")]
         public DateTime dataHora { get; set; }
     }
 }
