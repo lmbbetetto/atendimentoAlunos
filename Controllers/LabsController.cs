8643f627ccd0a42c4ef6885cdc9af1d4abb0b384
Leonardo Betetto <60801645+lmbbetetto@users.noreply.github.com>
2023-09-05 21:55:50 -0300

    commit

---
+++ Controllers/LabsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using atendimentoAlunos.Models;
+
+namespace atendimentoAlunos.Controllers
+{
+    public class LabsController : Controller
+    {
+        private readonly Contexto _context;
+
+        public LabsController(Contexto context)
+        {
+            _context = context;
+        }
+
+
+        public async Task<IActionResult> Index() {
+                return View(await _context.Salas.ToListAsync());
+            
+        }
+
+    }
+}
--- Models/Contexto.cs
+++ Models/Contexto.cs
@@ -10,5 +10,6 @@ namespace atendimentoAlunos.Models
         public DbSet<Sala> Sala { get; set; }
         public DbSet<Aluno> Aluno { get; set; }
         public DbSet<Atendimento> Atendimento { get; set; }
+        public DbSet<Sala> Salas { get; set; }
     }
 }
